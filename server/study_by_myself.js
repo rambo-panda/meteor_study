21c5689878beee8293f41b4520953a12300da745
rambo <liluwei123@163.com>
2014-09-29 23:29:02 +0800

    bak the add route suport before

--- server/study_by_myself.js
+++ server/study_by_myself.js
@@ -1,14 +1,10 @@
 /*global Players:true, Random:true */
-"use strict";
 Players = new Mongo.Collection("players");
 
 Meteor.startup(function () {
 	if (Players.find().count() === 0) {
 		var names = [
 			"Ada Lovelace",
-			"Grace Hopper",
-			"Marie Curie",
-			"Carl Friedrich Gauss",
 			"Nikola Tesla",
 			"Claude Shannon"
 		];
@@ -16,4 +12,31 @@ Meteor.startup(function () {
 			Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
 		}
 	}
+
+
+
+	/*
+		*  It is used in version 0.9.3
+	Router.map(function() {
+		this.route('Home', {path:'/'}, function () {
+		// render the Home template with a custom data context
+		this.render('Home', {data: {title: 'My Title'}});
+		});
+
+		// when you navigate to "/one" automatically render the template named "One".
+		this.route('One', {path : '/one'});
+
+		// when you navigate to "/two" automatically render the template named "Two".
+		this.route('Two', {path : '/two'});
+	});
+	*/
+
+	Router.route('/', function () {
+		console.log('---');
+		this.render('./../client/Home', {data: {title: 'My Title'}});
+	});
+
+	Router.route('/one');
+
+	Router.route('/two');
 });
